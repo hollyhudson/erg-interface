47f9d1c98c82554180cf4366d9cc90047c15185b
Holly Hudson <commerce+gitlab@hollyhudson.net>
2021-02-12 18:53:40 +0100

    adds responsivenessgit add erg_live.js !

--- dashboard/erg_live.js
+++ dashboard/erg_live.js
@@ -20,51 +20,77 @@
 
 const canvas_width = 2048; // (170) 853 | 342 | 853
 const canvas_height = 1536; // (102) 1125/411 | 1536 | 714/411/411
+const speedometer_height = Math.round(canvas_height * 11 / 15);
+const meter_width = Math.round(canvas_width * 5 / 12);
+const meter_height = Math.round(canvas_height * 4 / 15);
+const power_bar_width = Math.round(canvas_width * 2 /12);
+const p_curve_height = Math.round(canvas_height * 7 / 12);
 
 let dashboard = d3.select("#dashboard").append("svg")
 	.attr("width", canvas_width)
-	.attr("height", canvas_height);
+	.attr("height", canvas_height)
+	.call(responsive_resize);
 
 let speedometer = dashboard.append("rect")
 	.attr("x", 0)
 	.attr("y", 0)
-	.attr("width", 853)
-	.attr("height", 1125)
+	.attr("width", meter_width)
+	.attr("height", speedometer_height)
 	.attr("fill", "cyan");
 
 let distance_meter = dashboard.append("rect")
 	.attr("x", 0)
-	.attr("y", 1125)
-	.attr("width", 853)
-	.attr("height", 411)
+	.attr("y", speedometer_height)
+	.attr("width", meter_width)
+	.attr("height", meter_height)
 	.attr("fill", "blue");
 
 let power_bar = dashboard.append("rect")
-	.attr("x", 853)
+	.attr("x", meter_width)
 	.attr("y", 0)
-	.attr("width", 342)
+	.attr("width", power_bar_width)
 	.attr("height", canvas_height)
 	.attr("fill", "orange");
 
 let power_curve = dashboard.append("rect")
-	.attr("x", 1195)
+	.attr("x", meter_width + power_bar_width)
 	.attr("y", 0)
-	.attr("width", 853)
-	.attr("height", 714)
+	.attr("width", meter_width)
+	.attr("height", p_curve_height)
 	.attr("fill", "pink");
 	
 let power_meter = dashboard.append("rect")
-	.attr("x", 1195)
-	.attr("y", 714)
-	.attr("width", 853)
-	.attr("height", 411)
+	.attr("x", meter_width + power_bar_width)
+	.attr("y", p_curve_height)
+	.attr("width", meter_width)
+	.attr("height", meter_height)
 	.attr("fill", "red");
 
 let time_meter = dashboard.append("rect")
-	.attr("x", 1195)
-	.attr("y", 1125)
-	.attr("width", 853)
-	.attr("height", 411)
+	.attr("x", meter_width + power_bar_width)
+	.attr("y", p_curve_height + meter_height)
+	.attr("width", meter_width)
+	.attr("height", meter_height)
 	.attr("fill", "white");
 	
+function responsive_resize(svg) {
+	// find the aspect ratio of the area being resized
+	const container = d3.select(svg.node().parentNode),
+		width = parseInt(svg.style("width"), 10),
+		height = parseInt(svg.style("height"), 10),
+		aspect = width / height;
 
+	svg
+		.attr("viewBox",`0 0 ${width} ${height}`)
+		.attr("preserveAspectRatio", "xMinYMid")
+		.call(resize);
+		
+		d3.select(window).on("resize." + container.attr("id"), resize);
+
+		function resize() {
+			const target_width = parseInt(container.style("width"))
+			svg
+				.attr("width", target_width)
+				.attr("height", Math.round(target_width / aspect));
+		}
+}
