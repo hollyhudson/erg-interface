3804465b67ec7b358d0904c44d34811843b5505b
Holly Hudson <commerce+gitlab@hollyhudson.net>
2021-02-15 23:30:22 +0100

    displays total power, time, and cadence

--- dashboard/erg_live.js
+++ dashboard/erg_live.js
@@ -11,13 +11,23 @@
  * split (text) inside speed meter  |  bar   |
  *                                  |        |  total power (text & bar)
  * ---------------------------------|        |--------------------------
- * total distance                   |        |  total time
+ * cadence (spm)                    |        |  total time
  *      ::: 5 :::                     ::2::          ::: 5 :::
  *
  */
 
-// 1920 x 1080 (16x9)
 
+let start_time = 0;
+let inst_power = 0; 
+let total_power = 0; 
+let stroke_power = 0; 
+let stroke_time = 0;
+let tick_duration = 0;
+let inst_spm = 0;
+let total_strokes = 0;
+let current_speed = 0;
+
+// 1920 x 1080 (16x9)
 const canvas_width = 1920; // (170) 853 | 342 | 853
 const canvas_height = 1080; // (102) 1125/411 | 1536 | 714/411/411
 const speedometer_height = Math.round(canvas_height * 11 / 15);
@@ -35,7 +45,7 @@ let speedometer = dashboard.append("svg")
 	.attr("x", 0)
 	.attr("y", 0);
 
-speedometer
+let speed_text = speedometer
 	.append("text")
 		.style("fill", "#0ff")
 		.style("font", "25px monospace")
@@ -44,18 +54,18 @@ speedometer
 		.attr("text-anchor", "middle")
 		.text("speedometer goes here");
 
-let distance_meter = dashboard.append("svg")
+let cadence_meter = dashboard.append("svg")
 	.attr("x", 0)
 	.attr("y", speedometer_height);
 
-distance_meter
+let cadence_text = cadence_meter
 	.append("text")
 		.style("fill", "#8af")
 		.style("font", "25px monospace")
 		.attr("x", meter_width/2)
 		.attr("y", meter_height/2)
 		.attr("text-anchor", "middle")
-		.text("distance goes here");
+		.text("cadence goes here");
 
 let power_bar = dashboard.append("svg")
 	.attr("x", meter_width)
@@ -87,7 +97,7 @@ let power_meter = dashboard.append("svg")
 	.attr("x", meter_width + power_bar_width)
 	.attr("y", p_curve_height);
 
-power_meter
+let power_text = power_meter
 	.append("text")
 		.style("fill", "#fa0")
 		.style("font", "25px monospace")
@@ -100,7 +110,7 @@ let time_meter = dashboard.append("svg")
 	.attr("x", meter_width + power_bar_width)
 	.attr("y", p_curve_height + meter_height);
 
-time_meter
+let time_text = time_meter
 	.append("text")
 		.style("fill", "#fff")
 		.style("font", "25px monospace")
@@ -109,9 +119,95 @@ time_meter
 		.attr("text-anchor", "middle")
 		.text("time goes here");
 
+// connect to the rower's websocket and append any new data as it comes in
+let host = location.hostname;
+
+//if (!host)
+	host = "192.168.178.51";
+
+let connection = new WebSocket('ws://' + host + ':81/', ['arduino']);
+connection.onopen = function () { connection.send('Connect ' + new Date()); };
+connection.onerror = function (error) { console.log('WebSocket Error ', error);};
+
+// update with new data as received
+connection.onmessage = function (e) {
+	console.log('Server: ', e.data);
+	if (e.data == "Connected")
+		return;
+
+	new_data = e.data.split(',');
+
+	// update variables
+	stroke_time = new_data[1];
+	inst_spm = new_data[5] / 10.0;
+	tick_duration = new_data[2];
+	inst_power = new_data[3] / 5000000.0;
+	stroke_power = new_data[4] / 5000000.0;
+
+
+	/******  DISPLAY TIME ******/
+
+	// if this is the first time through the loop, create a time offset
+	if(start_time == 0) {
+		start_time = (new_data[0]); 	
+	}
+
+	microsec = new_data[0] - start_time;
+	// to get sec divide by 1,000,000
+	total_sec = microsec / 1000000;
+	// to get min from this divide by 60
+	min = total_sec / 60;
+	// to get seconds from that mod 60
+	sec = total_sec % 60;
+
+	let display_min = parseInt(min);
+	let display_sec = parseInt(sec);
+
+	// left pad
+	if (display_min == 0)
+		display_min = "00";
+	else
+	if (display_min < 10)
+		display_min = "0" + display_min;
+
+	if (display_sec == 0)
+		display_sec = "00";
+	else
+	if (display_sec < 10)
+		display_sec = "0" + display_sec;
+
+	time_text.text(display_min + ":" + display_sec);
+	
+	/******  DISPLAY CADENCE ******/
+		
+	if (stroke_time == 0)
+	{
+		total_strokes++;
+	}
+	
+	cadence_text.text(parseInt(inst_spm));
+
+	/******  DISPLAY POWER ******/
+
+	// update total power
+	if (tick_duration > 0) {
+		total_power += inst_power;
+	}
+
+	power_text.text(parseInt(total_power) + " effort units"); 
+	
+	/******  DISPLAY SPEED ******/
+	
+	// speed is calculated here by effort/min
+	
+
+	/******  DISPLAY SPLIT ******/
+	
+	// splits are calculated here by seconds/600 units of effort
+};
+
 function responsive_resize(svg) {
 	// find the aspect ratio of the area being resized
-	console.log("in responsive_resive");
 	const container = d3.select(svg.node().parentNode),
 		width = parseInt(svg.style("width"), 10),
 		height = parseInt(svg.style("height"), 10),
